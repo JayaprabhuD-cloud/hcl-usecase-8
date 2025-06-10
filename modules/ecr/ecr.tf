12aaca1ac9dec9eaa2071bfe2d3e21501fe8adbb
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-10 18:16:46 +0000

    Adding few modules

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,6 +1,6 @@
 # Creating first repo for patient docker image
 
-resource "aws_ecr_repository" "Patient " {
+resource "aws_ecr_repository" "Patient" {
   name                 = var.repo_name_1
   image_tag_mutability = "MUTABLE"
 
@@ -14,7 +14,7 @@ resource "aws_ecr_repository" "Patient " {
 
 # Creating first repo for appointment docker image
 
-resource "aws_ecr_repository" "appointment " {
+resource "aws_ecr_repository" "appointment" {
   name                 = var.repo_name_2
   image_tag_mutability = "MUTABLE"
 
